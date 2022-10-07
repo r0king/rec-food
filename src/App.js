36e3e6813cd28cb6e9cfe0cb6254925ce91b4492
Royal <48954882+r0king@users.noreply.github.com>
2022-10-07 10:13:26 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import "animate.css/animate.min.css";
+import Card from './componets/Card';
+import Install from './componets/Install';
+
+function App() {
+  return (
+  
+  <div>
+    <Install/>    
+    {/* <h1 className="animate__animated animate__fadeInLeft animate__delay-faster ">An animated element</h1>
+    <Card className="animate__animated animate__fadeInLeft animate__delay-faster " ></Card> */}
+  </div>
+  );
+}
+
+export default App;
---
+++ src/componets/Install.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Install extends Component {
+  render() {
+    return (
+      <div className='bg-netral'>Install</div>
+    )
+  }
+}
+
+export default Install
