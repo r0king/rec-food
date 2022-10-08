a9cb41f0507a74390962cc49f40d1f3e6524d07b
kng <kng@pop-os.localdomain>
2022-10-08 20:07:22 +0530

    basic template

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import "animate.css/animate.min.css";
-import Card from './componets/Card';
-import Install from './componets/Install';
+import Card from "./componets/Card";
+import Install from "./componets/Install";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-  
-  <div>
-    <Install/>    
-    {/* <h1 className="animate__animated animate__fadeInLeft animate__delay-faster ">An animated element</h1>
-    <Card className="animate__animated animate__fadeInLeft animate__delay-faster " ></Card> */}
-  </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<Card />} />
+        <Route path="/table" element={<Install />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/componets/Hero.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+
+export class Hero extends Component {
+  render() {
+    return (
+      <div className="hero min-h-screen bg-base-200 bg-cover bg-blend-overlay " style={{ backgroundImage: `url("https://source.unsplash.com/hrlvr2ZlUNk/")` }}>
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <p className="py-6">
+              Search through the most delicious and awsome food recipies available in seconds
+            </p>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <a href="#home" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Hero;
